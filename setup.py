e26f6e51b755da9eed0a91a82721ab01409b570d
Charles Leifer <coleifer@gmail.com>
2018-03-14 15:29:38 -0500

    0.3.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ extension = Extension(
 
 setup(
     name='sweepea',
-    version='0.3.0',
+    version='0.3.1',
     description='',
     url='https://github.com/coleifer/sweepea',
     install_requires=['Cython'],
